de156cd4e95a324a7ec858a929473176f22276d8
Darrien <darrienpatriot@gmail.com>
2022-12-21 22:18:14 -0500

    Added async as well as pipeline for answers

--- ETL/schema.sql
+++ ETL/schema.sql
@@ -1,8 +1,9 @@
-CREATE DATABASE questiontest;
+SELECT 'CREATE DATABASE questiontest'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname='questiontest')\gexec
 
 \connect questiontest;
 
-CREATE TABLE questions (
+CREATE TABLE IF NOT EXISTS questions (
   id SERIAL PRIMARY KEY,
   product_id int,
   body VARCHAR(255),
@@ -12,3 +13,17 @@ CREATE TABLE questions (
   reported int,
   helpful int
 );
+
+CREATE TABLE IF NOT EXISTS answers(
+  id SERIAL PRIMARY KEY,
+  question_id int,
+  body VARCHAR(255),
+  date_written VARCHAR(50),
+  answerer_name VARCHAR(50),
+  answerer_email VARCHAR(100),
+  reported int,
+  helpful int,
+  CONSTRAINT fk_question
+    FOREIGN KEY(question_id)
+      REFERENCES questions(id)
+)
