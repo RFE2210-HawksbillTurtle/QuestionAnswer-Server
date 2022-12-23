c74d4863c5e1d0a6cc23e484e2ec579b038a2e57
Darrien <darrienpatriot@gmail.com>
2022-12-22 20:17:18 -0500

    added post question route

--- app/controllers/questions.js
+++ app/controllers/questions.js
@@ -34,10 +34,20 @@ exports.getAllAnswers = async (req, res, next) => {
   }
 };
 
-exports.getOne = async (req, res, next) => {
+exports.addOneQuestion = async (req, res, next) => {
+  console.log(req.body)
   try {
-    const question = await Question.findByPk(req.params.id);
-    return res.status(200).json(question);
+    const question = await Question.create({
+      product_id: req.body.product_id,
+      body: req.body.body,
+      date_written: req.body.date_written,
+      asker_name: req.body.asker_name,
+      asker_email: req.body.asker_email,
+      reported: 0,
+      helpful: 0
+
+    });
+    return res.status(201).json(question);
   } catch (err) {
     return res.status(500).json(err);
   }
--- app/routes/questions.js
+++ app/routes/questions.js
@@ -5,8 +5,8 @@ const router = require('express').Router();
 //CRUD
 router
    .get('/:product_id', controller.getAllQs)
-   .get('/:question_id/answers', controller.getAllAnswers);
-
+   .get('/:question_id/answers', controller.getAllAnswers)
+   .post('/', controller.addOneQuestion);
 
 
 module.exports = router;
