105a5254a7dba8754efcebb50687208a2672228c
Darrien <darrienpatriot@gmail.com>
2023-01-03 17:44:13 -0500

    Wrote unit tests for /:product_id && /:question_id/answers

--- app/tests/questions.test.js
+++ app/tests/questions.test.js
@@ -1,9 +1,9 @@
 const chakram = require('chakram');
 expect = chakram.expect;
 
-describe('Questions', () => {
-  it('should respond with a 200', () => {
-    let response = chakram.get("http://localhost:3000/qa/questions/37311");
+describe('/qa/questions/:product_id', () => {
+  it('should respond with a 200', async () => {
+    let response = await chakram.get("http://localhost:3000/qa/questions/37311");
     return expect(response).to.have.status(200);
   })
 
@@ -13,7 +13,37 @@ describe('Questions', () => {
       expect(json.results).to.be.an('array');
       expect(json.results.length).to.be.above(0);
      })
-
-
   })
+
+  it('should return an empty array when product doesn\'t exist', async () => {
+    let response = await chakram.get("http://localhost:3000/qa/questions/9999999")
+   return expect(response).to.have.json((json) => {
+     expect(json.results).to.be.an('array');
+     expect(json.results.length).to.eq(0);
+    })
+  })
+})
+
+describe('qa/questions/:question_id/answers', () => {
+  it('should respond with a 200', async () => {
+    let response = await chakram.get("http://localhost:3000/qa/questions/131232/answers")
+    return expect(response).to.have.status(200);
+  })
+
+  it('should have data in results array', () => {
+    let response = chakram.get("http://localhost:3000/qa/questions/131232/answers")
+    return expect(response).to.have.json((json) => {
+      expect(json.results).to.be.an('array');
+        expect(json.results.length).to.be.above(0);
+    })
+  })
+
+  it('should return an empty array when question exist', async () => {
+    let response = await chakram.get("http://localhost:3000/qa/questions/99999999/answers")
+    return expect(response).to.have.json((json) => {
+      expect(json.results).to.be.an('array');
+      expect(json.results.length).to.eq(0);
+    })
+  })
+
 })
