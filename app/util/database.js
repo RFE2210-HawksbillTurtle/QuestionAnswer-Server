10f3321c695c528d01f588df2b8c8e9f7412e884
Darrien <darrienpatriot@gmail.com>
2023-01-05 16:26:32 -0500

    changed info

--- app/util/database.js
+++ app/util/database.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize');
 const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 
-const sequelize = new Sequelize(process.env.PGDB, process.env.PGUSER, process.env.PASS, {
+const sequelize = new Sequelize(process.env.PGDB, process.env.PGUSER, process.env.PGPASS, {
   dialect: 'postgres',
-  host: 'localhost',
+  host: process.env.PGHOST,
   logging: false
 });
 
